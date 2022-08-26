5d73e97d536cda8577a9a17afdb79fd15e85bcc9
Maksym Horbachevsky <maximgorb@gmail.com>
2022-08-26 20:27:29 +0300

    Started JavaScript

---
+++ JavaScript/Basics/5/code.js
@@ -0,0 +1,7 @@
+"use strict";
+
+// Comment
+alert("This is a double slash comment ")
+
+/*This comment*/
+alert("This is a star comment")
---
+++ JavaScript/Basics/6/code.js
@@ -0,0 +1,21 @@
+"use strict";
+
+let num;
+num = 42;
+// alert(num);
+
+let h = 1;
+let z, b, c;
+z = 1;
+b = 2;
+c = 3;
+
+let s = 2, t = 5, q = 4;
+
+
+let a = 10;
+alert(a);
+
+a=20;
+alert(a);
+
